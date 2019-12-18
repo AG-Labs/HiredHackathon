07b38425b5eacc69f8621ea9c8490816736549fd
Andrew Godley <agdevlabs@gmail.com>
2019-12-18 22:22:24 +0000

    corrected dice throws for code smells

--- dice-51.py
+++ dice-51.py
@@ -2,16 +2,16 @@
 #n times and provides the outcome
 
 import random
-def rollDiceX(diceThrows):
+def roll_dice_x(dice_throws):
 	output = []
 
-	for count in range(0, diceThrows):
-		diceThrow = random.randint(0, 5)
-		diceThrow += 1
-		output.append(diceThrow)
+	for count in range(0, dice_throws):
+		dice_throw = random.randint(0, 5)
+		dice_throw += 1
+		output.append(dice_throw)
 	return output
 
-result = rollDiceX(10)
+result = roll_dice_x(10)
 
 print("your results are ", result)
 
